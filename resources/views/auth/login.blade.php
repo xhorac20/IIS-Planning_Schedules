0992cf3adb13f01277032bc2cc0201910e955bea
xhorac20 <xhorac20@stud.fit.vutbr.cz>
2023-11-21 23:35:04 +0100

    Login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title', 'Prihlásenie')
+
+@section('content')
+    <div class="login-container">
+        <!-- Nadpis -->
+        <h1 class="login-heading">Školský Rozvrh</h1>
+        <!-- Formular Prihlasenia -->
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <label for="login">Login:</label>
+                <input id="login" type="text" class="form-control" name="login" required autofocus>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Heslo:</label>
+                <input id="password" type="password" class="form-control" name="password" required>
+            </div>
+
+            <div class="button-group">
+                <button type="submit" class="btn btn-dark-blue">
+                    {{ __('Login') }}
+                </button>
+                <a href="{{ route('register') }}" class="btn btn-dark-blue">
+                    {{ __('Register') }}
+                </a>
+                <a href="{{ url('/') }}" class="btn btn-dark-blue">
+                    {{ __('Hosť') }}
+                </a>
+            </div>
+        </form>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Default Title')</title>
-    <!-- Zde můžete přidat odkazy na CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body>
 <header>
     <!-- Hlavička aplikace -->
-    <!-- Implementace navigační lišty (předpokládá se použití Bootstrapu) -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Školní Rozvrh</a>
-            <!-- Další prvky navigace -->
-        </div>
-    </nav>
 </header>
 
 <main>
@@ -27,11 +17,11 @@
 
 <footer>
     <!-- Pata aplikace -->
+    <div class="footer-content">
+        <p>Vytvorené tímom xhorac20: Andrej Horáček (xhorac20), Jakub Brčiak (xbrcia00), Matěj Novák (xnovak2v)</p>
+        <p>Názov projektu: Plánování rozvrhů</p>
+    </div>
 </footer>
 
-<!-- Zde můžete přidat odkazy na JavaScript -->
-<script src="{{ asset('js/bootstrap.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
 </body>
-
 </html>
